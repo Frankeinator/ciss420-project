7e941c32490466d297213c4dd1599e53c6d52f56
arfranke1 <144175459+arfranke1@users.noreply.github.com>
2024-01-10 16:29:37 -0600

    pp

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from flask import Flask
 import random
 
+html = f'''
+        <h1> Random Number: </h1>
+        <hr>
+        <ul>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+        </ul>
+        <hr>
+        <ol>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+            <li>{random.randint(1, 5)}</li>
+        </ol>
+
+        '''
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    
-    return f'{r}'
+    return html
 
 if __name__ == '__main__':
     app.run()
